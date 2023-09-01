a55c86b74c176590306f57310169d0d3ffb1d24b
aimfeld <adrian.imfeld@brightanswer.ch>
2023-09-01 09:56:31 +0200

    basic reactive plot

--- app.R
+++ app.R
@@ -4,22 +4,25 @@ df_hdi <- read.csv('data/hdi.csv')
 
 min_year <- min(df_hdi$year)
 max_year <- max(df_hdi$year)
+
 indicators <- c(
-    'University Score' = 'uni_score',
-    'Human Development Index' = 'hdi',
-    'Life Expectancy at Birth' = 'le',
-    'Expected Years of Schooling' = 'eys',
-    'Mean Years of Schooling' = 'mys',
-    'Gross National Income Per Capita' = 'gnipc'
+    'uni_score' = 'University Score',
+    'hdi' = 'Human Development Index',
+    'le' = 'Life Expectancy at Birth',
+    'eys' = 'Expected Years of Schooling',
+    'mys' = 'Mean Years of Schooling',
+    'gnipc' = 'Gross National Income Per Capita'
 )
+# Flip values and names for input choices
+indicators_input <- setNames(names(indicators), indicators)
 
 # UI
 ui <- fluidPage(
     titlePanel("University Scores and Human Development Index"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("xAxis", 'x-Axis', indicators, selected = 'hdi'),
-            selectInput("yAxis", 'y-Axis', indicators, selected = 'uni_score')
+            selectInput("xAxis", 'x-Axis', indicators_input, selected = 'hdi'),
+            selectInput("yAxis", 'y-Axis', indicators_input, selected = 'uni_score')
         ),
         mainPanel(
             plotOutput("plot"),
@@ -27,18 +30,23 @@ ui <- fluidPage(
                 "year", label = "Year",
                 min = min_year, value = min_year, max = max_year, 
                 step = 1, ticks = FALSE, sep = ""
-            ),
-            textOutput("xOutput")
+            )
         )
     )
 )
 
-## Set up back-end logic (server)
+## Server
 server <- function(input, output, session) {
-  ## The following would work
-  output$xOutput <- renderText({
-    paste("x-Axis selected:", input$xAxis)
-  })
+    # Combine the selected variables into a new data frame
+    selectedData <- reactive({
+        df_hdi[df_hdi$year == input$year, c(input$xAxis, input$yAxis)]
+    })
+    # Plot
+    output$plot <- renderPlot({
+        plot(selectedData(), 
+             xlab = indicators[input$xAxis],
+             ylab = indicators[input$yAxis])
+    })
 }
 
 ## Put both together. You can also click on RStudio's "Run App" button.
