88c8e8a3d7b499b90b5c627aa093b18f3b03fedf
aimfeld <adrian.imfeld@brightanswer.ch>
2023-09-01 10:47:17 +0200

    KPI limits

--- app.R
+++ app.R
@@ -2,27 +2,27 @@ library(shiny)
 
 df_hdi <- read.csv('data/hdi.csv')
 
+kpis <- c(
+    'University Score' = 'uni_score',
+    'Human Development Index' = 'hdi',
+    'Life Expectancy at Birth (years)' = 'le',
+    'Expected Years of Schooling (years)' = 'eys',
+    'Mean Years of Schooling (years)' = 'mys',
+    'Gross National Income Per Capita (2017 PPP$)' = 'gnipc'
+)
+
+# Calculate limits
 min_year <- min(df_hdi$year)
 max_year <- max(df_hdi$year)
-
-indicators <- c(
-    'uni_score' = 'University Score',
-    'hdi' = 'Human Development Index',
-    'le' = 'Life Expectancy at Birth',
-    'eys' = 'Expected Years of Schooling',
-    'mys' = 'Mean Years of Schooling',
-    'gnipc' = 'Gross National Income Per Capita'
-)
-# Flip values and names for input choices
-indicators_input <- setNames(names(indicators), indicators)
+kpi_limits <- ceiling(apply(df_hdi[, kpis], 2, function(x) max(x, na.rm = TRUE)))
 
 # UI
 ui <- fluidPage(
     titlePanel("University Scores and Human Development Index"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("xAxis", 'x-Axis', indicators_input, selected = 'hdi'),
-            selectInput("yAxis", 'y-Axis', indicators_input, selected = 'uni_score')
+            selectInput("xAxis", 'x-Axis', kpis, selected = 'hdi'),
+            selectInput("yAxis", 'y-Axis', kpis, selected = 'uni_score')
         ),
         mainPanel(
             plotOutput("plot"),
@@ -41,11 +41,16 @@ server <- function(input, output, session) {
     selectedData <- reactive({
         df_hdi[df_hdi$year == input$year, c(input$xAxis, input$yAxis)]
     })
+    
     # Plot
     output$plot <- renderPlot({
-        plot(selectedData(), 
-             xlab = indicators[input$xAxis],
-             ylab = indicators[input$yAxis])
+        df <- selectedData()
+        plot(df, 
+             xlab = names(kpis)[kpis == input$xAxis],
+             ylab = names(kpis)[kpis == input$yAxis],
+             xlim = c(0, kpi_limits[input$xAxis]),
+             ylim = c(0, kpi_limits[input$yAxis])
+        )
     })
 }
 
